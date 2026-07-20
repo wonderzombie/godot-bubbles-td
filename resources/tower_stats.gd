1947ba128b1522e52ad6c69a6b27288fc50d66f1
wz <wonderzombie@gmail.com>
2026-07-19 20:21:41 -0700

    rearrange declarations

--- resources/tower_stats.gd
+++ resources/tower_stats.gd
@@ -1,14 +1,16 @@
 class_name TowerStats extends Resource
 
-@export var detection_radius: float = 36.0
-@export var attack_cooldown: float = 2.05
-@export var hits_per_rock: int = 2
-@export var rock_range: int = 12 * 3 * 1 + 6
-
 enum Ty {
 	DOG_I,
 }
 
-static var STATS = {
+
+static var all_stats = {
 	Ty.DOG_I: load("res://resources/dog_tower_i.tres")
 }
+
+
+@export var detection_radius: float = 36.0
+@export var attack_cooldown: float = 2.05
+@export var hits_per_rock: int = 2
+@export var rock_range: float = 12 * 4
