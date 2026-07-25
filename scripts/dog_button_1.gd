e302dd43a24fe882c29f44b303a0efd8001b9ba6
wz <wonderzombie@gmail.com>
2026-07-24 17:14:20 -0700

    add cost labels to toolbar
    add "hover" visual change

--- scripts/dog_button_1.gd
+++ scripts/dog_button_1.gd
@@ -4,3 +4,20 @@ class_name DogButton extends Sprite2D
 @export var cost: int = 15
 
 @onready var scene := load(tower_file)
+@onready var label: Label = get_node(^"CostLabel")
+@onready var default_alpha: float = self.self_modulate.a
+
+func _ready() -> void:
+	label.text = "%d" % cost
+	self.self_modulate.a = default_alpha
+
+func _unhandled_input(event: InputEvent) -> void:
+	var mouse_event := event as InputEventMouseMotion
+
+	if !mouse_event:
+		return
+
+	if self.get_rect().has_point(get_local_mouse_position()):
+		self.self_modulate.a = 1.0
+	elif self.self_modulate.a != default_alpha:
+		self.self_modulate.a = default_alpha
